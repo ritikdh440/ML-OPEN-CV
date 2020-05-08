75b3c96fbb03b3e5e593ffe2dcb46f4ccc691216
Ritik dhiman <57491718+ritikdh440@users.noreply.github.com>
2020-05-08 18:26:49 +0530

    Add files via upload

--- OPEN CV/Adaptive_thresholding.py
+++ OPEN CV/Adaptive_thresholding.py
@@ -3,7 +3,7 @@ import cv2
 
 img = cv2.imread('/home/ritik/Documents/input images/crossword.jpg',0)
 
-_,th_1 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+_,th_1 = cv2.threshold(img, 200, 255, cv2.THRESH_BINARY)
 th_2 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 12);
 th_3 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 12);
 
